e8ac899aa4701d18d513957ebc61b08a3d5d03d8
Santiago Videla <santiago.videla@gmail.com>
2014-03-24 12:45:03 +0100

    post-release

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@
 from setuptools import setup, find_packages
 import sys, os
 
-version = '2.0.0dev'
+version = '2.0.1dev'
 
 long_desc = """
 The aim of caspo is to implement a pipeline for automated reasoning on logical signaling networks. 
