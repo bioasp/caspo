ec649f019c8929fa36cf4a1767ee45318073f9ec
Santiago Videla <santiago.videla@gmail.com>
2014-07-31 15:56:40 +0200

    fix tests

--- caspo/learn/tests/learn_graphadapters.rst
+++ caspo/learn/tests/learn_graphadapters.rst
@@ -23,16 +23,15 @@ Let's check everything has been parsed correctly::
     >>> ('b',-1) in graph1.predecessors('c')
     True
 
+Lines and tabs can be mixed::
+
+    >>> graph2 = core.IGraph(fake_sif2)
+    >>> ('b','c',-1) in graph2.edges
+    True
+
 Next, let's check that invalid SIF files are well handled. First a file providing non integer relation between nodes::
 
     >>> graph3 = core.IGraph(fake_sif_err1)
     Traceback (most recent call last):
     ...
     IOError: Cannot read line a	activates	c in SIF file: invalid literal for int() with base 10: 'activates'
-    
-Second, a line not separated by tabs::
-
-    >>> graph4 = core.IGraph(fake_sif_err2)
-    Traceback (most recent call last):
-    ...
-    IOError: Cannot read line b	-1 c in SIF file: need more than 2 values to unpack
--- caspo/learn/tests/learn_graphadapters_fixt.py
+++ caspo/learn/tests/learn_graphadapters_fixt.py
@@ -8,6 +8,13 @@ class MockSIFReader(object):
         lines = ["a\t1\tc","b\t-1\tc","c\t1\td"]
         return iter(lines)
 
+class MockSIFReader2(object):
+    interface.implements(core.IFileReader)
+    
+    def __iter__(self):
+        lines = ["a\t1\tc","b\t-1 c","c\t1\td"]
+        return iter(lines)
+
 class MockSIFReaderErr1(object):
     interface.implements(core.IFileReader)
     
@@ -15,16 +22,9 @@ class MockSIFReaderErr1(object):
         lines = ["a\tactivates\tc","b\t-1\tc","c\t1\td"]
         return iter(lines)
 
-class MockSIFReaderErr2(object):
-    interface.implements(core.IFileReader)
-    
-    def __iter__(self):
-        lines = ["a\t1\tc","b\t-1 c","c\t1\td"]
-        return iter(lines)
-
 def setup_test(test):
     test.globs['fake_sif'] = MockSIFReader()
+    test.globs['fake_sif2'] = MockSIFReader2()
     test.globs['fake_sif_err1'] = MockSIFReaderErr1()
-    test.globs['fake_sif_err2'] = MockSIFReaderErr2()
     
 setup_test.__test__ = False
