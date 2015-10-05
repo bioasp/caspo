6cb81b2afc06c51559d6458c83bbf539ba5c4d92
Santiago Videla <santiago.videla@gmail.com>
2015-10-05 01:20:18 -0300

    clean init in design

--- caspo/design/__init__.py
+++ caspo/design/__init__.py
@@ -15,43 +15,5 @@
 # You should have received a copy of the GNU General Public License
 # along with caspo.  If not, see <http://www.gnu.org/licenses/>.
 # -*- coding: utf-8 -*-
-import os
-
-from interfaces import *
-from utilities import *
-from adapters import *
-from impl import *
-
-from pyzcasp import asp, potassco
-from caspo import core
-
-from zope import component
-
-gsm = component.getGlobalSiteManager()
-
-gsm.registerAdapter(NetworksSetup2TermSet)
-gsm.registerAdapter(NetworksSetupClampings2TermSet)
-gsm.registerAdapter(PotasscoDesigner)
-gsm.registerAdapter(ClampingList2CsvWriter)
-gsm.registerAdapter(ClampingListEntropy)
-
-root = os.path.dirname(__file__)
-reg = component.getUtility(asp.IEncodingRegistry)
-reg.register('caspo.design.iopt', os.path.join(root, 'encodings/gringo4/idesign.lp'), potassco.IGringo4)
-reg.register('caspo.design.opt', os.path.join(root, 'encodings/gringo4/design.lp'), potassco.IGringo4)
-
-reg = component.getUtility(asp.IArgumentRegistry)
-reg.register('caspo.design.iopt', ['-c maxstimuli={stimuli}', 
-                                  '-c maxinhibitors={inhibitors}', 
-                                  '-c imax={imax}'],               potassco.IGringoGrounder)
-                                  
-reg.register('caspo.design.opt', ['-c maxstimuli={stimuli}', 
-                                  '-c maxinhibitors={inhibitors}', 
-                                  '-c nexp={nexp}'],               potassco.IGringoGrounder)
-
-reg.register('caspo.design.opt', ["--quiet=1", "--opt-mode=optN"], potassco.IClasp3)
-
-def register_mt(threads, conf="many"):
-    reg.register('caspo.design.opt', ["--quiet=1", "--opt-mode=optN", "--conf=%s" % conf, "-t %s" % threads], potassco.IClasp3)
 
 from design import designer
