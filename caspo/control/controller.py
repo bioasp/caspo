0fc1c3c40ebbb8e9412ab9eb631cdb795960b7fc
Santiago Videla <santiago.videla@gmail.com>
2016-03-30 23:55:13 -0300

    blanks

--- caspo/control/controller.py
+++ caspo/control/controller.py
@@ -29,28 +29,28 @@ class Controller(object):
     def __init__(self, networks, scenarios):
         self.networks = networks
         self.scenarios = scenarios
-        
-        
+
+
         fs = networks.to_funset().union(scenarios.to_funset())
         for v in it.ifilter(lambda n: n not in scenarios.exclude, networks.hg.nodes['name']):
             fs.add(gringo.Fun("candidate",[v]))
-            
+
         self.instance = ". ".join(map(str, fs)) + ". #show intervention/2."
-        
+
         root = os.path.dirname(__file__)
         self.encodings = {
             'control':    os.path.join(root, 'encodings/encoding.lp')
         }
-        
+
         self.logger = logging.getLogger("caspo")
-        
+
     def __save__(self,model):
         tuples = (f.args() for f in model.atoms())
         self.strategies.append(core.Clamping.from_tuples(tuples))
-            
+
     def control(self, size=0, configure=None):
         self.strategies = []
-        
+
         clingo = gringo.Control(['-c maxsize=%s' % size])
 
         clingo.conf.solve.models = '0'
@@ -59,23 +59,21 @@ class Controller(object):
                 for i in xrange(args.threads):
                     proxy.solver[i].heuristic = 'domain'
                     proxy.solver[i].dom_mod = '5,16'
-                    
+
             configure(clingo.conf, overwrite)
         else:
             clingo.conf.solver.heuristic = 'domain'
             clingo.conf.solver.dom_mod = '5,16'
 
         clingo.conf.solve.enum_mode = 'domRec'
-                
+
         clingo.add("base", [], self.instance)
         clingo.load(self.encodings['control'])
-        
+
         clingo.ground([("base", [])])
         clingo.solve(on_model=self.__save__)
-        
+
         n,t = len(self.strategies), clingo.stats['time_total']
         self.logger.info("%s optimal intervention strategies found in %.4fs" % (n,t))
-        
-        self.strategies = core.ClampingList(self.strategies)
 
-        
+        self.strategies = core.ClampingList(self.strategies)
