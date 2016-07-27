eda0873f6587d957dcff2079c753abf3aac93f1c
Santiago Videla <santiago.videla@gmail.com>
2016-07-27 12:05:54 +0200

    mock gringo

--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,7 @@ import shlex
 import mock
  
 MOCK_MODULES = ['numpy', 'scipy', 'matplotlib', 'pandas', 
-                'seaborn', 'sklearn', 'sklearn.metrics']
+                'seaborn', 'sklearn', 'sklearn.metrics', 'gringo']
 
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = mock.Mock()
